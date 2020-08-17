8627d6028026d1cfd27cf84081aefda9165a776e
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2020-08-18 00:32:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,6 +18,31 @@ $(document).ready(function () {
 });
 
 
+//scroll down arrow
+
+$(document).ready(function () {
+    $('a[href^="#technology"]').on('click', function(event) {
+        event.preventDefault();
+        let sc = $(this).attr("href"),
+            dn = $(sc).offset().top;
+        $('html, body').animate({scrollTop: dn}, 500);
+    });
+
+    $('a[href^="#extension"]').on('click', function(event) {
+        event.preventDefault();
+        let sc = $(this).attr("href"),
+            dn = $(sc).offset().top;
+        $('html, body').animate({scrollTop: dn}, 1000);
+    });
+
+    $('a[href^="#feedback"]').on('click', function(event) {
+        event.preventDefault();
+        let sc = $(this).attr("href"),
+            dn = $(sc).offset().top;
+        $('html, body').animate({scrollTop: dn}, 1000);
+    });
+});
+
 
 
 
