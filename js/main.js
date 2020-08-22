2bd261355a98f3290c4f543acb9e073b55eb3bbe
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2020-08-22 21:49:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,16 +5,6 @@ $(document).ready(function () {
         $('.header-burger__menu, .site-navigation').toggleClass('active');
         $('body').toggleClass('lock');
     });
-
-    $('.site-navigation__cross').click(function (event) {
-        $('.header-burger__menu, .site-navigation').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-
-    $('.burger-menu').click(function (event) {
-        $('.drop-menu-burger').slideToggle();
-        $('.burger-menu').toggleClass('arrow-rotate');
-    });
 });
 
 
